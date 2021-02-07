06c7453f9ab776235a7c3f7b7cf68b36048fbe87
Otakar Vydra <vydraotakar@gmail.com>
2021-02-07 17:36:34 +0100

    Added logic for incomes and added data output, final output is a dictionary with keys being the individual sellers and value the amount spent via them.

--- expenditure_summary_script.py
+++ expenditure_summary_script.py
@@ -1,20 +1,10 @@
 #Imports
-
 import csv
+import numpy as np
 
-#Define empty categories for summing expenditure
-food      = 0
-fuel      = 0
-household = 0
-parking   = 0
-education = 0
-apps      = 0
-hygiene   = 0
-tea       = 0
-social    = 0
-tools     = 0
-car       = 0
-bike      = 0
+#Define empty lists
+sellers = []
+amounts = []
 
 #Import the csv file and parse
 column_1 = []
@@ -28,7 +18,6 @@ with open('file.csv', newline='') as file_1:
         column_2.append(line['column_2'])
 
 #Engine
-
 for i in range(len(column_1)):
     string = column_2[i]
     string = string.strip(';')
@@ -36,7 +25,11 @@ for i in range(len(column_1)):
     if string_list[0] == '00 CZK':
         string           = column_1[i]
         string_list      = string.split(';')
-        amount           = int(string_list[1].strip('-'))
+        amount           = int(string_list[1])
+        if amount > 0:
+            sellers.append('income')
+            amounts.append(amount)
+            continue
         seller           = string_list[9]
         transaction_list = string_list[13].split('(')
         transaction_type = transaction_list[0]
@@ -45,12 +38,41 @@ for i in range(len(column_1)):
     else:
         string           = column_2[i]
         string_list      = string.split(';')
-        amount           = int(column_1[i].split(';')[1].strip('-')) + int(string_list[0].strip('0')) / 100
+        amount_1         = int(column_1[i].split(';')[1])
+        if amount_1 > 0:
+            try:
+                amount = amount_1 + int(column_2[i].split(';')[0]) / 100
+            except ValueError:
+                amount = amount_1
+            sellers.append('income')
+            amounts.append(amount)
+            continue
+        amount = amount_1 - int(column_2[i].split(';')[0]) / 100
         seller           = string_list[8] 
         transaction_list = string_list[12].split('(')
         transaction_type = transaction_list[0]
         transaction_date = transaction_list[1].strip(')')
 
+    if transaction_type == 'Výb?r z bankomatu':
+        seller = 'bankomat'
 
+    sellers.append(seller)
+    amounts.append(amount)
 
+#Data output
+sellers_unique = np.unique(sellers)
 
+sellers_f = []
+amounts_f = []
+
+for seller_u in sellers_unique:
+    sum_1 = 0
+    for i in range(len(sellers)):
+        if sellers[i] == seller_u:
+            sum_1 += amounts[i]
+    print('{sum_1} via {seller_u}'.format(sum_1 = sum_1, seller_u = seller_u))
+    sellers_f.append(seller_u)
+    amounts_f.append(sum_1)
+
+#Final dict creation
+final_dict = {key : value for key, value in zip(sellers_f, amounts_f)}
